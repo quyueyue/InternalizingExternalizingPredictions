608ccfe47a4e555a296727cac5fcc92c5f276117
Lydia Qu <78581023+quyueyue@users.noreply.github.com>
2023-05-15 13:15:01 -0400

    Update CBIG_TRBPC_multiKRR_create_features.sh

--- ABCD_KRR_code/multiKRR/CBIG_TRBPC_multiKRR_create_features.sh
+++ ABCD_KRR_code/multiKRR/CBIG_TRBPC_multiKRR_create_features.sh
@@ -2,4 +2,4 @@
 
 outdir=/gpfs/milgram/project/holmes/yq222/FYP/ABCD/
 
-matlab -nodesktop -nosplash -nodisplay -r "CBIG_TRBPC_multiKRR_get_inputs('$outdir');exit;"
+matlab -nodesktop -nosplash -nodisplay -r "CBIG_TRBPC_multiKRR_feature_file('$outdir');exit;"
