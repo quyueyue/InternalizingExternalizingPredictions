67c7f05d8abb369adcca48e05be896af173cb32f
Lydia Qu <78581023+quyueyue@users.noreply.github.com>
2023-05-15 13:28:28 -0400

    Update CBIG_TRBPC_multiKRR_create_features.sh

--- ABCD/ABCD_KRR_code/multiKRR/CBIG_TRBPC_multiKRR_create_features.sh
+++ ABCD/ABCD_KRR_code/multiKRR/CBIG_TRBPC_multiKRR_create_features.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-outdir=${IntExt_Dir}/ABCD_multiKRR_inputs ## The directory where you want to store FC_all.mat which will be fed as an input to CBIG_TRBPC_multiKRR_LpOCV_workflow_slurm.sh
+outdir=${IntExt_Dir}/ABCD/ABCD_data_inputs ## The directory where you want to store FC_all.mat which will be fed as an input to CBIG_TRBPC_multiKRR_LpOCV_workflow_slurm.sh
 analysis_type='multiKRR'
 
 matlab -nodesktop -nosplash -nodisplay -r "CBIG_TRBPC_feature_file('$analysis_type','$outdir');exit;"
