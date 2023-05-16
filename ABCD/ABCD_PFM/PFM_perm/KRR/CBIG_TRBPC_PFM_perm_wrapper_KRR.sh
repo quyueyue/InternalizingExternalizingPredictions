c86cc549c632397e132f0de8168fbfcfbeae67a9
Lydia Qu <78581023+quyueyue@users.noreply.github.com>
2023-05-16 16:06:44 -0400

    Update CBIG_TRBPC_PFM_perm_wrapper_KRR.sh

--- ABCD/ABCD_PFM/PFM_perm/KRR/CBIG_TRBPC_PFM_perm_wrapper_KRR.sh
+++ ABCD/ABCD_PFM/PFM_perm/KRR/CBIG_TRBPC_PFM_perm_wrapper_KRR.sh
@@ -57,8 +57,8 @@ date >> $LF
 ########## prepare inputs for the permutation
 scripts_dir=`dirname "$(readlink -f "$0")"`
 
-#matlab -nodesktop -nosplash -nodisplay -r " addpath $scripts_dir; CBIG_TRBPC_prepare_PFM_perm_inputs_KRR( \
-#   '$feature_file', '$input_dir', '$outdir'); exit; " >> $LF 2>&1
+matlab -nodesktop -nosplash -nodisplay -r " addpath $scripts_dir; CBIG_TRBPC_prepare_PFM_perm_inputs_KRR( \
+  '$feature_file', '$input_dir', '$outdir'); exit; " >> $LF 2>&1
    
 ########## run permutation jobs
 y_file=$outdir/y_regress.mat
