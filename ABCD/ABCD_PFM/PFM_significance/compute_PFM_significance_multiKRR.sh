6c03bf4a2dfea51db30278ddb7cd81f11c01ec85
Lydia Qu <78581023+quyueyue@users.noreply.github.com>
2023-05-16 15:09:34 -0400

    Update compute_PFM_significance_multiKRR.sh

--- ABCD/ABCD_PFM/PFM_significance/compute_PFM_significance_multiKRR.sh
+++ ABCD/ABCD_PFM/PFM_significance/compute_PFM_significance_multiKRR.sh
@@ -4,9 +4,12 @@
 #SBATCH --mem=20G
 #SBATCH --partition psych_day
 
+# perm_dir="/gpfs/milgram/project/holmes/yq222/FYP/permutation_2262/PFM/"
 
-perm_dir="/gpfs/milgram/project/holmes/yq222/FYP/permutation_2262/PFM/"
-outdir=${perm_dir}
 scripts_dir=`dirname "$(readlink -f "$0")"`
+
+perm_dir=$IntExt_Dir/ABCD_PFM_perm_output/PFM/KRR/
+
+outdir=${perm_dir}
 
 matlab -nodesktop -nosplash -nodisplay -r " addpath ${scripts_dir}; CBIG_TRBPC_compute_PFM_significance_mask_multiKRR_wrapper('${perm_dir}','${outdir}'); exit; "
