a903304dedd39eff06a2b63a0f1639385c4d3eb2
Lydia Qu <78581023+quyueyue@users.noreply.github.com>
2023-05-15 20:04:32 -0400

    Update CBIG_TRBPC_tested_config_adapted.sh

--- setup/CBIG_TRBPC_tested_config_adapted.sh
+++ setup/CBIG_TRBPC_tested_config_adapted.sh
@@ -7,9 +7,10 @@ if [ -n "$FREESURFER_HOME" ]; then
 	$FREESURFER_HOME/bin/clear_fs_env.csh
 fi
 
-# PLEASE CHANGE: Please specify location of CBIG repository
+# PLEASE CHANGE: Please specify location of CBIG repository and IntExt repository
 export CBIG_CODE_DIR=/gpfs/milgram/project/holmes/yq222/FYP/CBIG/
 export IntExt_DIR=/gpfs/milgram/project/holmes/yq222/FYP/
+
 # PLEASE CHANGE: define locations for these libraries
 export FREESURFER_HOME=/apps/hpc.rhel7/software/FreeSurfer/5.3.0/
 export CBIG_MATLAB_DIR=/apps/hpc.rhel7/software/MATLAB/2018b/
@@ -20,7 +21,7 @@ export CBIG_WB_DIR=/apps/hpc.rhel7/software/ConnectomeWorkbench/1.4.2/
 export CBIG_CARET_DIR=/apps/hpc.rhel7/software/Caret/
 export CBIG_FSLDIR=/apps/hpc.rhel7/software/FSL/6.0.4-centos7_64/
 
-# DO NOT CHANGE: define locations for unit tests data and replication data
+# PLEASE CHANGE: define locations for unit tests data and replication data
 export CBIG_TESTDATA_DIR=$CBIG_CODE_DIR/stable_projects/predict_phenotypes/ChenTam2022_TRBPC/unit_tests
 export CBIG_REPDATA_DIR=$CBIG_CODE_DIR/stable_projects/predict_phenotypes/ChenTam2022_TRBPC/replication
 
@@ -31,7 +32,7 @@ export CBIG_SCHEDULER_DIR=/apps/sysapps/TORQUE/bin
 SETUP_PATH=$CBIG_CODE_DIR/setup/CBIG_generic_setup.sh
 source $SETUP_PATH
 
-# DO NOT CHANGE: set up temporary directory for MRIread from FS6.0
+# PLEASE CHANGE: set up temporary directory for MRIread from FS6.0
 export TMPDIR=/gpfs/milgram/project/holmes/yq222/FYP/Thomas_replication/tmpstore
 
 # Do NOT CHANGE: set up MATLABPATH so that MATLAB can find startup.m in our repo
