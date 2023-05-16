5fc77987889db1e0b97a10300b5ec06ffe0c13d6
Lydia Qu <78581023+quyueyue@users.noreply.github.com>
2023-05-16 14:00:50 -0400

    Update prepare_PFM_perm_inputs_KRR.sh

--- ABCD/ABCD_PFM/PFM_perm/KRR/prepare_PFM_perm_inputs_KRR.sh
+++ ABCD/ABCD_PFM/PFM_perm/KRR/prepare_PFM_perm_inputs_KRR.sh
@@ -4,9 +4,13 @@
 #SBATCH --mem=30G
 #SBATCH --partition psych_day
 
-feature_file="/gpfs/milgram/project/holmes/yq222/FYP/ABCD/FC_subjects_all_task_all_score_rs_bp_sm6.mat"
-input_dir="/gpfs/milgram/project/holmes/yq222/FYP/ABCD_output_rs_KRR/"
-outdir="/gpfs/milgram/project/holmes/yq222/FYP/permutation_2262/KRR/PFM/"
+# feature_file="/gpfs/milgram/project/holmes/yq222/FYP/ABCD/FC_subjects_all_task_all_score_rs_bp_sm6.mat"
+# input_dir="/gpfs/milgram/project/holmes/yq222/FYP/ABCD_output_rs_KRR/"
+# outdir="/gpfs/milgram/project/holmes/yq222/FYP/permutation_2262/KRR/PFM/"
+feature_file=$IntExt_Dir/ABCD_data_inputs/FC_subjects_all_task_all_score_rs_bp_sm6.mat
+input_dir=$IntExt_Dir/ABCD_KRR_output/
+outdir=$IntExt_Dir/ABCD_perm_output/PFM_perm/KRR/
+
 
 ########## prepare inputs for the permutation
 scripts_dir=`dirname "$(readlink -f "$0")"`
