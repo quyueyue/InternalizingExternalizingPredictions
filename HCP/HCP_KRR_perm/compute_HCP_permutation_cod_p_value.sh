f74b72452868f728c2f41b1b97e071499481d62d
Lydia Qu <78581023+quyueyue@users.noreply.github.com>
2023-05-17 00:14:16 -0400

    Update compute_HCP_permutation_cod_p_value.sh

--- HCP/HCP_KRR_perm/compute_HCP_permutation_cod_p_value.sh
+++ HCP/HCP_KRR_perm/compute_HCP_permutation_cod_p_value.sh
@@ -4,9 +4,14 @@
 #SBATCH --mem=20G
 #SBATCH --partition psych_day
 
-regression_results_dir="/gpfs/milgram/project/holmes/yq222/FYP/HCP/HCP_output_KRR/"
+# CHANGE: please specify the directory containing the single-KRR outputs
+# regression_results_dir="/gpfs/milgram/project/holmes/yq222/FYP/HCP/HCP_output_KRR/"
+regression_results_dir="$IntExt_DIR/HCP/HCP_output_KRR/"
+# DON'T CHANGE: behavioral indices (18 behaviors in total)
 behav_ind=1:18
+# DON'T CHANGE: outstem
 outstem="KRR_features_rs_752"
+# CHANGE: please specify the directory containing single-KRR permutation results
 perm_dir="/gpfs/milgram/project/holmes/yq222/FYP/HCP/permutations/KRR_features_rs_752/"
 seeds_total=60
 
