1b0bb0cd9e342ff61c5d9c14d2fa797b2410680f
quyueyue <quyueyue1996@gmail.com>
2024-08-13 13:22:36 -0400

    Remove PFM correlation tests

--- ABCD/ABCD_PFM_corr_diff/KRR/compute_PFM_corr_perm_int_ext_KRR.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -J compute_PFM_corr_perm_int_ext
-#SBATCH -t 3:00:00
-#SBATCH --mem=30G
-#SBATCH --partition psych_day
-
-module load miniconda
-conda activate py3_env
-python PFM_corr_permutation_int_ext_KRR.py
