6bbf250a66473b5b371fc8c19972bea30a0047b4
Lydia Qu <yq222@login1.milgram.ycrc.yale.edu>
2024-08-13 16:53:49 -0400

    Commit HCP PFM code

---
+++ HCP/HCP_PFM/PFM_computation/CBIG_TRBPC_PFM_job_HCP.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Written by Jianzhong Chen and CBIG under MIT license: https://github.com/ThomasYeoLab/CBIG/blob/master/LICENSE.md
+
+###########################################
+# Usage and Reading in Parameters
+###########################################
+input_dir=$1
+feature_file=$2
+score_ind=$3
+outdir=$4
+model=$5
+
+########## compute the PFM
+LF="$outdir/logs/compute_${model}_PFM.log"
+date >> $LF
+scripts_dir=`dirname "$(readlink -f "$0")"`
+matlab -nodesktop -nosplash -nodisplay -r " addpath $scripts_dir; CBIG_TRBPC_compute_${model}_PFM_HCP( \
+'$feature_file', '$input_dir', '$score_ind', '$outdir' );\
+exit; " >> ${outdir}/logs/${model}_PFM_score_${score_ind}.txt
