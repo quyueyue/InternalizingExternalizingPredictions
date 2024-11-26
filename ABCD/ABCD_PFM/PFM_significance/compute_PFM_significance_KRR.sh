52217ab98293038b346e41fe31dbf3de789221de
Lydia Qu <78581023+quyueyue@users.noreply.github.com>
2024-11-25 20:19:09 -0500

    Update compute_PFM_significance_KRR.sh

--- ABCD/ABCD_PFM/PFM_significance/compute_PFM_significance_KRR.sh
+++ ABCD/ABCD_PFM/PFM_significance/compute_PFM_significance_KRR.sh
@@ -4,14 +4,10 @@
 #SBATCH --mem=20G
 #SBATCH --partition psych_day
 
+## CHANGE THIS
 perm_dir="/gpfs/milgram/project/holmes/yq222/FYP/permutation_5260/KRR/PFM/"
+## CHANGE THIS; ASSUMING THAT THE SIGNIFICANCE MASK GOES TO THE SAME DIRECTORY AS THE PFM PERMUTATION RESULTS
 outdir=${perm_dir}
 scripts_dir=`dirname "$(readlink -f "$0")"`
-
-#scripts_dir=`dirname "$(readlink -f "$0")"`
-
-#perm_dir=$IntExt_Dir/ABCD/ABCD_perm_output/PFM/KRR/
-
-outdir=${perm_dir}
 
 matlab -nodesktop -nosplash -nodisplay -r " addpath ${scripts_dir}; CBIG_TRBPC_compute_PFM_significance_mask_KRR_wrapper('${perm_dir}','${outdir}'); exit; "
