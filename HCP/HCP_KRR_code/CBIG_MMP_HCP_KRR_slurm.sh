425a5c483f0dab23e8c633e8f68f9518d992031e
Lydia Qu <78581023+quyueyue@users.noreply.github.com>
2024-11-25 20:59:52 -0500

    Update CBIG_MMP_HCP_KRR_slurm.sh

--- HCP/HCP_KRR_code/CBIG_MMP_HCP_KRR_slurm.sh
+++ HCP/HCP_KRR_code/CBIG_MMP_HCP_KRR_slurm.sh
@@ -9,7 +9,7 @@
 # CHANGE: specify the general HCP analysis directory
 HCP_dir="$IntExt_DIR/HCP/"
 # CHANGE: specify the directory containing the resting-state FC data named "features_rs_423.mat"
-feature_path="$HCP_dir/fMRI_data/features_rs_423.mat"
+feature_path="$HCP_dir/HCP_data_inputs/features_rs_423.mat"
 # CHANGE: specify the directory containing the results of the single-KRR model
 outdir="$HCP_dir/HCP_output_KRR"
 # DON'T CHANGE: 10 outer cv folds
@@ -17,13 +17,13 @@ outerFolds=10
 # DON'T CHANGE: 10 inner cv folds
 innerFolds=10
 # CHANGE: specify the directory containing the list of 423 subjects named "HCP_423_subjects.txt"
-subtxt="$HCP_dir/behavioral_data/HCP_423_subjects.txt"
+subtxt="$HCP_dir/HCP_data_inputs/HCP_423_subjects.txt"
 # CHANGE: specify the directory containing a data frame with 1. Subject (ID); 2. Gender; 3. AgeInYrs; 4. ZygositySR; 5. Family_ID; 6. Race; 7. All ASR internalizing and externalizing scales;
-scorecsv="$HCP_dir/behavioral_data/HCP_alldata_filtered.csv"
+scorecsv="$HCP_dir/HCP_data_inputs/HCP_alldata_filtered.csv"
 # CHANGE: specify the directory containing the list of covariates (AgeinYrs and Gender)
-covtxt="$HCP_dir/behavioral_data/covariates_list.txt"
+covtxt="$HCP_dir/HCP_data_inputs/covariates_list.txt"
 # CHANGE: specify the directory containing the list of variables to predict (ASR variable names)
-predvar="$HCP_dir/behavioral_data/variables_to_predict_hcp.txt"
+predvar="$HCP_dir/HCP_data_inputs/variables_to_predict_hcp.txt"
 # Name of .mat containing the values of all y variables
 ymat="output_y.mat"
 # Name of .mat containing the values of all covariates
