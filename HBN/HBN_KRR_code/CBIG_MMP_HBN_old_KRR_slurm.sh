1a8970f4ad657b00bd77705b36bff46d711cf036
Lydia Qu <78581023+quyueyue@users.noreply.github.com>
2024-11-25 23:46:54 -0500

    Update CBIG_MMP_HBN_old_KRR_slurm.sh

--- HBN/HBN_KRR_code/CBIG_MMP_HBN_old_KRR_slurm.sh
+++ HBN/HBN_KRR_code/CBIG_MMP_HBN_old_KRR_slurm.sh
@@ -9,7 +9,7 @@
 # CHANGE: specify the general HBN analysis directory
 HBN_dir="$IntExt_DIR/HBN/"
 # CHANGE: specify the directory containing the resting-state FC data
-feature_path="$HBN_dir/fMRI_data/HBN_old_RSFC_combat.mat"
+feature_path="$HBN_dir/HBN_data_inputs/HBN_old_RSFC_combat.mat"
 # CHANGE: specify the directory containing the results of the single-KRR model
 outdir="$HBN_dir/HBN_old_output_KRR"
 # DON'T CHANGE: 10 outer cv folds
@@ -17,13 +17,13 @@ outerFolds=10
 # DON'T CHANGE: 10 inner cv folds
 innerFolds=10
 # CHANGE: specify the directory containing the list of 752 subjects named "HBN_752_subjects.txt"
-subtxt="$HBN_dir/behavioral_data/HBN_old_subjects.txt"
+subtxt="$HBN_dir/HBN_data_inputs/HBN_old_subjects.txt"
 # CHANGE: specify the directory containing a data frame with 1. Subject (ID); 2. Gender; 3. AgeInYrs; 4. ZygositySR; 5. Family_ID; 6. Race; 7. All ASR internalizing and externalizing scales; 8. mean FD (Framewise Displacement)
-scorecsv="$HBN_dir/behavioral_data/HBN_CBCL_old.csv"
+scorecsv="$HBN_dir/HBN_data_inputs/HBN_CBCL_old.csv"
 # CHANGE: specify the directory containing the list of covariates (AgeinYrs and FD_mean)
-covtxt="$HBN_dir/behavioral_data/covariates_list_HBN.txt"
+covtxt="$HBN_dir/HBN_data_inputs/covariates_list_HBN.txt"
 # CHANGE: specify the directory containing the list of variables to predict (ASR variable names)
-predvar="$HBN_dir/behavioral_data/CBCL_variables_to_predict_HBN.txt"
+predvar="$HBN_dir/HBN_data_inputs/CBCL_variables_to_predict_HBN.txt"
 # Name of .mat containing the values of all y variables
 ymat="output_y.mat"
 # Name of .mat containing the values of all covariates
