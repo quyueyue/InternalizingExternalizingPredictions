fae90f692d15299a44bc208b96b4061431b72f20
Lydia Qu <78581023+quyueyue@users.noreply.github.com>
2024-11-26 00:51:04 -0500

    Update HBN_PFM_perm.sh

--- HBN/HBN_PFM/PFM_perm/HBN_PFM_perm.sh
+++ HBN/HBN_PFM/PFM_perm/HBN_PFM_perm.sh
@@ -4,4 +4,4 @@
 #SBATCH --mem=30G
 #SBATCH --partition psych_day
 
-bash CBIG_TRBPC_PFM_perm_wrapper_HBN.sh -i $IntExt_DIR/HBN/HBN_old_output_KRR/ -f $IntExt_DIR/HBN/fMRI_data/HBN_old_RSFC_combat.mat -n 2000 -t 2000 -o $IntExt_DIR/HBN/HBN_old_output_KRR/permutations/PFM/ -p $IntExt_DIR/HBN/behavioral_data/HBN_CBCL_old.csv -l $IntExt_DIR/HBN/behavioral_data/HBN_old_subjects.txt -c Milgram
+bash CBIG_TRBPC_PFM_perm_wrapper_HBN.sh -i $IntExt_DIR/HBN/HBN_output_KRR/ -f $IntExt_DIR/HBN/fMRI_data/HBN_RSFC_combat.mat -n 2000 -t 2000 -o $IntExt_DIR/HBN/HBN_output_KRR/permutations/PFM/ -p $IntExt_DIR/HBN/behavioral_data/HBN_CBCL_old.csv -l $IntExt_DIR/HBN/behavioral_data/HBN_subjects.txt -c Milgram
